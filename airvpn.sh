358ca1d11d813cef2ef412794307e165363af39e
[sCRiPTz-TEAM] <scriptzteam@users.noreply.github.com>
2025-07-31 10:46:53 +0200

    Update airvpn.sh

--- airvpn.sh
+++ airvpn.sh
@@ -11,7 +11,7 @@ if [[ -z "$json" ]]; then
 fi
 
 # Print table header
-printf "%-12s | %-15s | %-10s | %-15s | %-8s | %-8s | %-6s | %-8s | %-40s | %-70s | %-6s\n" \
+printf "%-12s | %-15s | %-10s | %-15s | %-8s | %-8s | %-6s | %-8s | %-40s | %-70s | %-6s |\n" \
   "Public Name" "Country Name" "Country" "Location" "Bw(Mbps)" "MaxBw" "Users" "Load(%)" "IPv4 Addresses" "IPv6 Addresses" "Health"
 printf -- "--------------------------------------------------------------------------------------------------------------------------------------------------------------\n"
 
@@ -32,6 +32,6 @@ echo "$json" | jq -r '
     .health
   ] | @tsv
 ' | while IFS=$'\t' read -r name country_name country_code location bw bwmax users load ipv4 ipv6 health; do
-  printf "%-12s | %-15s | %-10s | %-15s | %-8s | %-8s | %-6s | %-8s | %-40s | %-70s | %-6s\n" \
+  printf "%-12s | %-15s | %-10s | %-15s | %-8s | %-8s | %-6s | %-8s | %-40s | %-70s | %-6s |\n" \
     "$name" "$country_name" "$country_code" "$location" "$bw" "$bwmax" "$users" "$load" "$ipv4" "$ipv6" "$health"
 done
